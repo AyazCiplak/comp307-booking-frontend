a7c7184030ebfde2a0df3acbe22546249a41ca89
rtalwar05 <talwarrhea@gmail.com>
2026-04-16 19:24:09 -0400

    form but not centered

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -11,6 +11,28 @@ function Register() {
           </div>
           <h1 className="title">Book SoCS</h1>
         </div>
+
+        {/* registration form */}
+        <div className="form">
+          <div className="row">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="half-width"
+            />
+            <input type="text" placeholder="Last Name" className="half-width" />
+          </div>
+          <input
+            type="email"
+            placeholder="McGill Email"
+            className="full-width"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="full-width"
+          />
+        </div>
       </main>
     </div>
   );
