e8a1289663b1f07002cd951ec829979edbed55ec
rtalwar05 <talwarrhea@gmail.com>
2026-04-17 12:53:58 -0400

    spacing fixed

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -2,37 +2,48 @@ import "./Register.css";
 
 function Register() {
   return (
-    <div className="container">
-      <main className="card">
+    <div className="register-page">
+      <main className="register-layout">
         {/* title and logo */}
-        <div className="header">
-          <div className="illustration">
-            <img src="/logo.png" alt="BookSoCS Logo" className="logo" />
+        <div className="register-header">
+          <div className="register-illustration">
+            <img
+              src="/logo.png"
+              alt="BookSoCS Logo"
+              className="register-logo"
+            />
           </div>
-          <h1 className="title">Book SoCS</h1>
+          <h1 className="register-title">Book SoCS</h1>
         </div>
 
         {/* registration form */}
-        <div className="form">
-          <div className="row">
+        <section className="register-form-shell">
+          <div className="register-form">
+            <h2 className="register-form-title">Register</h2>
+            <div className="register-row">
+              <input
+                type="text"
+                placeholder="First Name"
+                className="register-half-width"
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="register-half-width"
+              />
+            </div>
             <input
-              type="text"
-              placeholder="First Name"
-              className="half-width"
+              type="email"
+              placeholder="McGill Email"
+              className="register-full-width"
             />
-            <input type="text" placeholder="Last Name" className="half-width" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="register-full-width"
+            />
           </div>
-          <input
-            type="email"
-            placeholder="McGill Email"
-            className="full-width"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="full-width"
-          />
-        </div>
+        </section>
       </main>
     </div>
   );
