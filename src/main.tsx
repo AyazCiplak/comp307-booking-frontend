e082244d23553e5a76f0d028faffd02e233b3a5b
rtalwar05 <talwarrhea@gmail.com>
2026-04-20 16:36:24 -0400

    login page finished

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,19 @@ import Register from "./pages/auth/Register.tsx";
 import AuthLayout from "./pages/shared/AuthLayout.tsx";
 import Dashboard from "./pages/shared/Dashboard.tsx";
 import Temp from "./pages/public/temp/Temp.tsx";
+import Login from "./pages/auth/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider> {/* Makes auth state available */}
+    <AuthProvider>
+      {" "}
+      {/* Makes auth state available */}
       <BrowserRouter>
         <Routes>
           {/* Public routes — no navbar */}
           <Route path="/" element={<Landing />} />
           <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login />} />
 
           {/* Authenticated routes — Navbar rendered automatically via AuthLayout */}
           <Route element={<AuthLayout />}>
